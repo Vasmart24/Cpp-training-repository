0b527c30165e975004ce34489117bc7bbc00aa7e
Vasmart24 <vasmart24/@yandex.ru>
2023-02-25 17:56:15 +0300

    add Game_stats v1.0, reconf file sys

--- Chapter_1/Expensive Calculator/expensive_calculator.cpp
+++ Chapter_1/Expensive Calculator/expensive_calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 int main() {
   cout << "7 + 3 = " << 7 + 3 << endl;
---
+++ Chapter_1/Game Stats/Game_Stats.h
@@ -0,0 +1,14 @@
+//
+// Created by ВЧ on 25.02.2023.
+//
+
+#ifndef CODES_C_GAME_STATS_H
+#define CODES_C_GAME_STATS_H
+
+
+class Game_Stats {
+
+};
+
+
+#endif //CODES_C_GAME_STATS_H
