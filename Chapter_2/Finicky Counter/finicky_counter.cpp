b2404b88c09967f7c1169a66b935dabc8b4113ea
teregiray <dantonengier@mail.ru>
2023-03-04 15:21:52 +0300

    add Finicky Counter

---
+++ Chapter_2/Finicky Counter/finicky_counter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int count = 0;
+	while (true) {
+		count += 1;
+		if (count > 10) {
+			break;
+		}
+		if (count == 5) {
+			continue;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
