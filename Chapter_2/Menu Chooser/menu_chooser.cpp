cbdb20f54b7a69a88aea7e10cc7aad37a00e2a6a
Vasmart24 <vasmart24/@yandex.ru>
2023-03-04 18:35:09 +0300

    update menu chooser with enumerations

--- Chapter_2/Menu Chooser/menu_chooser.cpp
+++ Chapter_2/Menu Chooser/menu_chooser.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 int main() {
 	cout << "Difficulty Levels\n\n";
-	cout << "1 - Easy\n";
-	cout << "2 - Normal\n";
-	cout << "3 - Hard\n\n";
+	enum difficulties { EASY = 1, NORMAL, HARD };
 	int choice;
 	cout << "Choice: ";
 	cin >> choice;
 	switch (choice) {
-		case 1:
+		case EASY:
 			cout << "You picked Easy. \n";
 		break;
-		case 2:
+		case NORMAL:
 			cout << "You picked Normal. \n";
 		break;
-		case 3:
-			cout << "You picked Hard. \n";
+		case HARD:
+      cout << "You picked Hard. \n";
 		break;
 		default:
 			cout << "You made and illegal choice. \n";
