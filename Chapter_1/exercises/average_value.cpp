7d6dc6b43eedfbd306afec554eee6263d8498d54
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-03-04 13:17:20 +0300

    change average_value

--- Chapter_1/exercises/average_value.cpp
+++ Chapter_1/exercises/average_value.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
   unsigned int firstPoints, secondPoints, thirdPoints;
-  cout << "write first score: ";
+  cout << "Write first score: ";
   cin >> firstPoints;
-  cout << "write second score: ";
+  cout << "Write second score: ";
   cin >> secondPoints;
-  cout << "write third score: ";
+  cout << "Write third score: ";
   cin >> thirdPoints;
   cout << "\nYour average Points is " << (firstPoints + secondPoints + thirdPoints) / 3.0 << endl;
   return 0;
