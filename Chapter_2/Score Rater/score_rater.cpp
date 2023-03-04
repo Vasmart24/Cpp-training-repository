0e9b3f84723e44bab4b31f25897bc71416ca6d37
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-03-04 14:15:19 +0300

    Score Rater v.2

--- Chapter_2/Score Rater/score_rater.cpp
+++ Chapter_2/Score Rater/score_rater.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	if (true) {
-		cout << "This is always displayed.\n\n";
-	}
-	if (false) {
-	  cout << "This is never displayed. \n\n";
-	}
-
-	int score = 1000;
-	if (score) {
-		cout << "At least you didn't score zero. \n\n";
-	}
-	if (score >= 250) {
-		cout << "You scored 250 or more. Decent. \n\n";
-	}
-	if (score >= 500) {
-		cout << "Your scored 500 or more. Nice. \n\n";
-		if (score >= 1000) {
-			cout << "You scored 100 or more. Impressive!\n";
-		}
+	int score;
+	cout << "Enter your score: ";
+	cin >> score;
+	if (score >= 1000) {
+	cout << "You scored 1000 or more. Impressive!\n";
+	} else {
+		cout << "You scored less than 1000.\n";
 	}
 return 0;
 }
