9fa1fe05a1f8c132be14b20cc9f3ab465464a628
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-03-19 18:00:12 +0300

    add main loop for commands

--- Chapter_4/exercises/favorite_games_list.cpp
+++ Chapter_4/exercises/favorite_games_list.cpp
@@ -12,4 +12,43 @@ int main() {
   << "command 'show', show your headers on the screen :)\n"
   << "command 'exit' throw you out from the game.";
   vector<string> headers;
+
+  do {
   cout << "Please, write your command: ";
+  switch (currCmd) {
+    case "show":
+      cout << "\nGames List: \n";
+      for (iter = headers.begin(); iter != headers.end(); ++iter) {
+        cout << *iter << endl;
+      }
+      break;
+    
+    case "add":
+      string gameName;
+      cout << "Please, write the header of the game: "
+      cin >> gameName;
+      iter = find(headers.begin(), headers.end(), gameName);
+      if (iter === headers.end()) {
+        cout << "This header is in list already." << endl;
+        break;
+      }
+      headers.insert(gameName);
+      cout << "The header succesfully added!";
+
+    case: "delete":
+      string gameName;
+      cout << "Please, write the header of the game: "
+      cin >> gameName;
+      iter = find(headers.begin(), headers.end(), gameName);
+      if (iter === headers.end()) {
+        cout << "This header doesn't exist in the list." << endl;
+        break;
+      }
+      headers.erase(gameName);
+      cout << "The header succesfully removed!";
+  }
+} while (currCmd !== "exit");
+
+cout << "Good Bye!" << endl;
+return 0;
+}
