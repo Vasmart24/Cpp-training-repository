bc4022bc945c787dc2c92adb2918418ba00791df
Vasmart24 <vasmart24/@yandex.ru>
2023-03-19 18:22:58 +0300

    fix data types, and change cases

--- Chapter_4/exercises/favorite_games_list.cpp
+++ Chapter_4/exercises/favorite_games_list.cpp
@@ -10,44 +10,47 @@ int main() {
   cout << "command 'add', make you add new game header to the list\n"
   << "command 'delete', make you delete any header from the list\n"
   << "command 'show', show your headers on the screen :)\n"
-  << "command 'exit' throw you out from the game.";
+  << "command 'exit' throw you out from the game.\n\n";
   vector<string> headers;
-
+  char currCmd;
+  string gameName;
   do {
-  cout << "Please, write your command: ";
-  switch (currCmd) {
-    case "show":
+    cout << "Please, write your command: ";
+    cin >> currCmd;
+    vector<string>::iterator iter;
+    switch (currCmd) {
+    case 'show':
       cout << "\nGames List: \n";
       for (iter = headers.begin(); iter != headers.end(); ++iter) {
         cout << *iter << endl;
       }
       break;
     
-    case "add":
-      string gameName;
-      cout << "Please, write the header of the game: "
-      cin >> gameName;
+    case 'add':
+        cout << "Please, write the header of the game: ";
+        cin >> gameName;
       iter = find(headers.begin(), headers.end(), gameName);
-      if (iter === headers.end()) {
+      if (iter == headers.end()) {
         cout << "This header is in list already." << endl;
         break;
       }
-      headers.insert(gameName);
-      cout << "The header succesfully added!";
+      headers.insert(headers.end(), gameName);
+      cout << "The header successfully added!";
+      break;
 
-    case: "delete":
-      string gameName;
-      cout << "Please, write the header of the game: "
+    case 'delete':
+      cout << "Please, write the header of the game: ";
       cin >> gameName;
       iter = find(headers.begin(), headers.end(), gameName);
-      if (iter === headers.end()) {
+      if (iter == headers.end()) {
         cout << "This header doesn't exist in the list." << endl;
         break;
       }
-      headers.erase(gameName);
-      cout << "The header succesfully removed!";
+      headers.erase(iter);
+      cout << "The header successfully removed!";
+      break;
   }
-} while (currCmd !== "exit");
+} while (currCmd != 'exit');
 
 cout << "Good Bye!" << endl;
 return 0;
