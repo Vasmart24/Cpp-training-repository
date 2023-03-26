c97ab9bb8d096cd6c769887937d6f5e1154d8868
Vasmart24 <vasmart24/@yandex.ru>
2023-03-26 18:02:13 +0300

    add Taking_Damage.cpp and .exe files

---
+++ Chapter_5/TakingDamage/Taking_Damage.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int radiation(int health);
+int main() {
+  int health = 80;
+  cout << "Your health is " << health << "\n\n";
+  health = radiation(health);
+  cout << "After radiation exposure your health is " << health << "\n\n";
+  health = radiation(health);
+  cout << "After radiation exposure your health is " << health << "\n\n";
+  health = radiation(health);
+  cout << "After radiation exposure your health is " << health << "\n\n";
+  return 0;
+}
+inline int radiation(int health) {
+  return (health / 2);
+}
