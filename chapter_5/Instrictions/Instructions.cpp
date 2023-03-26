b25639babd3ef9992f4a6335e66bb9aa125f1834
Vasmart24 <vasmart24/@yandex.ru>
2023-03-26 16:42:01 +0300

    add Instructions.cpp

---
+++ chapter_5/Instrictions/Instructions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void instructions();
+int main() {
+  instructions();
+  return 0;
+}
+void instructions() {
+  cout << "Welcome to the most fun you've ever had with text!"
+}
