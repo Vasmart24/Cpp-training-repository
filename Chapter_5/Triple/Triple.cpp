385ce26edbd9b2e2c13d80decce56b7de42928da
Vasmart24 <vasmart24/@yandex.ru>
2023-03-26 17:49:59 +0300

    add Triple.cpp

---
+++ Chapter_5/Triple/Triple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int triple(int number);
+string triple(string text);
+int main() {
+  cout << "\nTripling 5: " << triple(5) << "\n\n";
+  cout << "Tripling 'gamer': " << triple("gamer");
+  return 0;
+}
+int triple(int number) {
+  return (number * 3);
+}
+string triple(string text) {
+  return (text + text + text);
+}
